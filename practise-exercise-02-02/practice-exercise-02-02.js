3f4d4ee8efa482ef8ee1c30141a017874e314841
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2022-02-07 08:27:59 +0100

    Inital version of script for exercise 2.2.

---
+++ practise-exercise-02-02/practice-exercise-02-02.js
@@ -0,0 +1,8 @@
+let myName = "Henrik";
+let myAge = 48;
+let canCodeJavaScript = true;
+
+console.log(
+    `Hello, my name is ${myName}, I am ${myAge} years old and I can code` +
+    `JavaScript: ${canCodeJavaScript}`
+);
