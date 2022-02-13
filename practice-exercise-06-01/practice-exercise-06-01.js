d155df816e1d0082361ad839e68be2319721106e
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-02-13 08:03:17 +0100

    Add javascript for exercise 6.1.

---
+++ practice-exercise-06-01/practice-exercise-06-01.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+    let sum = x + y;
+    console.log(x + ' plus ' + y + ' equals ' + sum);
+}
+
+let nr1 = 1;
+let nr2 = 2;
+add(nr1, nr2);
+
+let nr3 = 3;
+let nr4 = 4;
+add(nr3, nr4);
