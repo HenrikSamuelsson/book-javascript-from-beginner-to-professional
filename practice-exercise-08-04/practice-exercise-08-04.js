ce5f6b6d77ba7e3e5209e4cf3c3eaa1a4d0871ae
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2022-02-17 13:54:14 +0100

    Add code to complet exercise 8.4.

--- practice-exercise-08-04/practice-exercise-08-04.js
+++ practice-exercise-08-04/practice-exercise-08-04.js
@@ -1,10 +1,19 @@
 const original = 'thIs will be capiTalized for each word';
+console.log(original);
 
 function capitalizeEachWord(originalString) {
     lowerCaseString = originalString.toLowerCase();
-    lowerCaseWords = lowerCaseString.split(' '); 
-
-    return lowerCaseWords;
+    lowerCaseWords = lowerCaseString.split(' ');
+    const capitalizedWords = [];
+    lowerCaseWords.forEach(element => {
+        firsLetter = element.slice(0, 1);
+        upperCaseFirsLetter = firsLetter.toUpperCase();
+        endOfWord = element.slice(1);
+        capitalizedWord = upperCaseFirsLetter + endOfWord;
+        capitalizedWords.push(capitalizedWord);
+    });
+    capitalizedString = capitalizedWords.join(' ');
+    return capitalizedString;
 }
 
 capitalizedString = capitalizeEachWord(original);
