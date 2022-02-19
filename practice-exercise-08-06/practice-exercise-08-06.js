912db39b72cf4e62d7e8c39efcb72e678d90d480
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-02-19 09:13:06 +0100

    Fix incorrect file name.

---
+++ practice-exercise-08-06/practice-exercise-08-06.js
@@ -0,0 +1,34 @@
+// Part 1 - Output of pi to the console using the Math object.
+console.log('Part 1');
+console.log('pi is ' + Math.PI);
+console.log('\n');
+
+// Part 2 - About ceil, floor, and rounding.
+console.log('Part 2');
+let number = 5.7;
+console.log('ceil of ' + number + ' is ' + Math.ceil(number));
+console.log('floor of ' + number + ' is ' + Math.floor(number));
+console.log('round of ' + number + ' is ' + Math.round(number));
+console.log('\n');
+
+// Part 3 - Generation of random number between 0 and 10 including 10.
+console.log("Part 4");
+console.log('generated number ' + Math.floor(Math.random() * 11));
+console.log('\n');
+
+// Part 4 - Generation of random number between 1 and 10 including 10.
+console.log("Part 5");
+console.log('generated number ' + Math.floor(Math.random() * 10 + 1));
+console.log('\n');
+
+// Part 5 - Generation of random number between 1 and 10 including 10.
+console.log("Part 5");
+console.log('generated number ' + Math.floor(Math.random() * 10 + 1));
+console.log('\n');
+
+// Part 5 - Generation of random number between 1 and 100 including 100.
+console.log("Part 5");
+console.log('generated number ' + Math.floor(Math.random() * 100 + 1));
+console.log('\n');
+
+// Part 6 - Generation of 100 random number between 1 to 100 using a function.
